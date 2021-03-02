fa725a0b5aa8dbcaef0722192d8f079137ff0207
Robert Konrad <git2016@robdangero.us>
2021-03-02 03:51:11 +0100

    Pimp datatypes

--- Sources/datatype.cpp
+++ Sources/datatype.cpp
@@ -5,23 +5,35 @@
 
 #include <Windows.h>
 
-Datatype loadDatatype(const char* name) {
+Datatype loadDatatype(const char *name) {
 	char path[MAX_PATH + 1];
 	strcpy(path, "Datatypes\\");
 	strcat(path, name);
 	HMODULE lib = LoadLibraryA(path);
+
 	Datatype datatype;
-	datatype.formats = (FormatsType)GetProcAddress(lib, "formats");
-	datatype.encode = (EncodeType)GetProcAddress(lib, "encode");
+	memset(&datatype, 0, sizeof(datatype));
+
+	if (lib != NULL) {
+		datatype.imageEncodeformats = (FormatsFunction)GetProcAddress(lib, "imageEncodeformats");
+		datatype.encodeImage = (ImageEncodeFunction)GetProcAddress(lib, "encodeImage");
+		datatype.imageDecodeformats = (FormatsFunction)GetProcAddress(lib, "imageDecodeformats");
+		datatype.decodeImage = (ImageDecodeFunction)GetProcAddress(lib, "decodeImage");
+
+		datatype.audioEncodeformats = (FormatsFunction)GetProcAddress(lib, "audioEncodeformats");
+		datatype.encodeAudio = (ImageEncodeFunction)GetProcAddress(lib, "encodeAudio");
+		datatype.audioDecodeformats = (FormatsFunction)GetProcAddress(lib, "audioDecodeformats");
+		datatype.decodeAudio = (ImageDecodeFunction)GetProcAddress(lib, "decodeAudio");
+	}
+
 	return datatype;
 }
 
 #else
 
-Datatype loadDatatype(const char* name) {
+Datatype loadDatatype(const char *name) {
 	Datatype datatype;
-	datatype.formats = NULL;
-	datatype.encode = NULL;
+	memset(&datatype, 0, sizeof(datatype));
 	return datatype;
 }
 
--- Sources/datatype.h
+++ Sources/datatype.h
@@ -1,11 +1,27 @@
 #pragma once
 
-typedef const char* (*FormatsType)();
-typedef void (*EncodeType)(int width, int height, int stride, const char* format, unsigned char* pixels, int* out_width, int* out_height, int* out_size, void** out_data);
+#include <stdint.h>
+
+typedef const char *(*FormatsFunction)();
+
+typedef void (*ImageDecodeFunction)(const char *format, uint8_t *data, size_t data_size, int *out_width, int *out_height, uint8_t **out_pixels,
+                                    size_t *out_size);
+typedef void (*ImageEncodeFunction)(const char *format, int width, int height, int stride, uint8_t *pixels, int *out_width, int *out_height, void **out_data,
+                                    int *out_size);
+
+typedef void (*AudioDecodeFunction)(const char *format, uint8_t *data, size_t data_size, uint8_t **out_data, size_t *out_size);
+typedef void (*AudioEncodeFunction)(const char *format, uint8_t *data, size_t data_size, uint8_t **out_data, size_t *out_size);
 
 struct Datatype {
-	FormatsType formats;
-	EncodeType encode;
+	FormatsFunction imageEncodeformats;
+	ImageEncodeFunction encodeImage;
+	FormatsFunction imageDecodeformats;
+	ImageDecodeFunction decodeImage;
+
+	FormatsFunction audioEncodeformats;
+	ImageEncodeFunction encodeAudio;
+	FormatsFunction audioDecodeformats;
+	ImageDecodeFunction decodeAudio;
 };
 
-Datatype loadDatatype(const char* name);
+Datatype loadDatatype(const char *name);
